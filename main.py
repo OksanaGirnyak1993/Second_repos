91cd2c66ee4862c89aea1c6e221837c15ddb54bd
OksanaGirnyak1993 <160180404+OksanaGirnyak1993@users.noreply.github.com>
2024-03-08 22:11:48 +0200

    дз 3 модуль

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello World")
+from datetime import datetime
+def get_days_until_date(date):
+    date = "2023-03-08"
+    input_date = datetime.strptime(date, "%Y-%m-%d")
+    current_date = datetime.now()
+    delta = input_date.toordinal() - current_date.toordinal() 
+    return delta
+print(get_days_until_date("2023-03-08"))
+
+
+  
+   
+
+
+    
+
+
