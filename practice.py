b8260a7faf07451b783e7a50e099b29ec0894a60
OksanaGirnyak1993 <160180404+OksanaGirnyak1993@users.noreply.github.com>
2024-03-11 14:39:39 +0200

    приклад

--- practice.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-# Створення об'єкта datetime
-now = datetime.now()
-
-# Використання isoweekday() для отримання дня тижня
-day_of_week = now.isoweekday()
-
-print(f"Сьогодні: {day_of_week}")  # Поверне число від 1 до 7, що відповідає дню тижня
